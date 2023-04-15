92cefe2e34161f9c7c881ece7606de94bf2e103e
Snadeep Tripathi <tripathisn5363@gmail.com>
2023-04-15 19:11:14 +0530

    moved git

---
+++ server2/src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const route = require("./routes/route");
+const app = express();
+const cors = require("cors");
+const multer = require("multer");
+mongoose.set({ strictQuery: true });
+require('dotenv').config();
+
+app.use(express.json());
+
+app.use(cors());
+app.use(multer().any());
+
+mongoose.connect(process.env.connectionString,
+    { dbName: "fsoc" },
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+app.use("/api", route);
+
+app.listen(process.env.port, function () {
+    console.log('Express app running on port ' + process.env.port)
+});
+
